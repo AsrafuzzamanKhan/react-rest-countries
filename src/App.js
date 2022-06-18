5f87b1c7dbbeb46b5510cc88253c7d7d8343087f
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2022-06-18 16:18:35 +0600

    refresh

--- src/App.js
+++ src/App.js
@@ -6,32 +6,9 @@ import Countries from './components/Countries/Countries';
 function App() {
   return (
     <div>
-      {/* <LoadCountries /> */}
       <Countries />
     </div>
   );
 }
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
-  return (
-    <div>
-      <h1>Load Countries:{countries.length}</h1>
-      {countries.map((country) => (
-        <Country name={country.name.common}></Country>
-      ))}
-    </div>
-  );
-}
-function Country(props) {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
-  );
-}
+
 export default App;
